a5a4bca9ca2ba60bbce5c5b18b754ea127db8fa4
Prathmesh Game <prathmeshgame2@gmail.com>
2026-07-23 15:20:34 +0530

    Add files via upload

---
+++ Programs.py
@@ -0,0 +1,150 @@
+
+
+                                                        #Below Average programs...
+#First Program
+#Write a PYTHON program that reads a value of n and check the number is zero or non zero value.
+
+'''
+n=int(input("Enter any Number:"))
+
+if(n>0):
+    print("Number is non zero")
+else:
+    print("The number is zero")
+'''
+
+#Second Program
+#Write a PYTHON program to find a largest of two numbers.
+'''
+
+a=int(input("Enter first Number:"))
+b=int(input("Enter second Number:"))
+
+if(a>b):
+    print(a," is greatest Number")
+elif(b>a):
+    print(b," is greatest Number")
+else:
+    print("Both Numbers are equal")
+'''
+
+#Third Program
+#Write a PYTHON program that reads the number and check the no is positive or negative.
+
+'''
+n=int(input("Enter any Number:"))
+
+if(n>0):
+    print("Number is positive ")
+elif(n<0):
+    print("The number is negative")
+else:
+    print("The number is zero")
+'''
+
+#Fourth Program
+#Write a PYTHON program to check entered character is vowel or consonant
+
+'''
+ch=input("Enter any Chracter:")
+
+if(ch=='a' or ch=='e'or ch=='i' or ch=='o' or ch=='u' or ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='U'):
+    print("The chracter is vowel")
+else:
+    print("The character is consonant")
+'''
+
+
+                                                            #Average Progams....
+#first Program
+'''
+Write a PYTHON program to evaluate the student performance
+      If % is >=90 then Excellent performance
+      If % is >=80 then  Very Good performance
+      If % is >=70 then Good performance
+      If % is >=60 then average performance
+      else Poor performance.
+'''
+
+'''
+m=int(input("Enter the Marks:"))
+if(m>=90):
+      print("Excellent Performans!!")
+elif(m>=80):
+    print("Very Good Performans!!")
+elif(m>=70):
+    print("Good Performans!!")
+elif(m>=60):
+    print("Average Performans!!")
+else:
+    print("Poor Performans!!")
+'''
+
+#Second Problem
+#Write a PYTHON program to find largest of three numbers.
+'''
+a=int(input("Enter first Number:"))
+b=int(input("Enter second Number:"))
+c=int(input("Enter third Number:"))
+if(a>b and a>c):
+    print(a," is greatest Number")
+elif(b>a and b>c):
+    print(b," is greatest Number")
+elif(c>a and c>b):
+    print(c," is greatest Number")
+else:
+    print("Both Numbers are equal")
+'''
+
+#Third Program
+#Write a PYTHON program to find smallest of three numbers
+
+'''
+a=int(input("Enter first Number:"))
+b=int(input("Enter second Number:"))
+c=int(input("Enter third Number:"))
+if(a<b and a<c):
+    print(a," is smallest Number")
+elif(b<a and b<c):
+    print(b," is smallet Number")
+elif(c<a and c<b):
+    print(c," is smmalles Number")
+else:
+    print("Both Numbers are equal")
+'''
+
+                                                                #Above Average Programs
+#First Program
+#Write a PYTHON program to check weather number is even or odd.
+
+'''
+a=int(input("Enter the Number:"))
+if(a%2==0):
+    print("Even number")
+else:
+    print("Odd Number")
+'''
+
+#Second Program
+#Write a PYTHON program to check a year for leap year.
+
+'''
+a=int(input("Enter the Number:"))
+if(a%4==0):
+    print("Leap Year")
+else:
+    print("Not a Leap Number")
+'''
+
+#Third Program
+
+'''
+married_status=input("enter marrietal status:")
+gender=input("enter gender male or female:")
+age=int(input("enter a age:"))
+        
+if married_status=="Married" or (married_status=="unmarried" and gender=="male" and age>30)or (married_status=="unmarried" and gender=="feamle" and age>25):
+    print("insure")
+else:
+    print("not insured:")
+'''
